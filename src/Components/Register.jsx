0810810eb1880876ea368db7d13358a4981adb7a
MursalinDevOps <mursalin1768@gmail.com>
2024-11-14 16:59:19 +0600

    register page added

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,7 +1,57 @@
+import { Link } from "react-router-dom";
+
+const handleRegister = (e) => {
+  e.preventDefault();
+
+  const name = e.target.name.value;
+  const email = e.target.email.value;
+  const password = e.target.password.value;
+  console.log(name, email, password)
+}
+
+
 export default function Register() {
   return (
-    <div>
-      
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex">
+        <div className="text-center lg:text-left">
+          <h1 className="text-3xl font-bold">Register now!</h1>
+        </div>
+        <div className="card bg-base-100 w-[600px] shrink-0 shadow-2xl">
+          <form onSubmit={handleRegister} className="card-body">
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input type="text" name="name" placeholder="name" className="input input-bordered" required />
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+          <p className="text-center mb-4">
+            Don't have an account ?
+            <Link className="underline"> Login Now</Link>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
